fb3fe4e749a46fd58e9962ccaa176a296d13db26
Javan <javanlam@gmail.com>
2026-04-08 09:28:04 +0800

    Updated dependencies and setup script

--- setup.sh
+++ setup.sh
@@ -9,10 +9,6 @@ conda create --name multimodal_rag
 eval "$(conda shell.bash hook)"
 conda activate multimodal_rag
 
-cd dinov2
-pip install -r requirements.txt
-
-cd ..
 pip install -r requirements.txt
 
 mv Long-CLIP LongCLIP
